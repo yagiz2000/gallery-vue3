b71033ddb830b9dbfa529d415172952d3ddf22a5
yagiz2000 <yagzceritoglu@gmail.com>
2022-07-23 20:23:35 +0300

    feat: initial commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret)
+
+import './assets/main.css'
+
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+
+app.mount('#app')
