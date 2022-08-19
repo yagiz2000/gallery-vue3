df3740586191b0d85cb46f8bc4eef163c7fdccb8
yagiz2000 <yagzceritoglu@gmail.com>
2022-08-19 15:53:39 +0300

    feat: font awesome package removed

--- src/main.js
+++ src/main.js
@@ -1,21 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
 
-/* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-/* import specific icons */
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
-library.add(faUserSecret)
 
-import './assets/main.css'
+import './assets/main.scss'
 
 const app = createApp(App)
-app.component('font-awesome-icon', FontAwesomeIcon)
 
 
 app.mount('#app')
