c92e18918a1eec09b44646425b1d9a47626bc9c1
yagiz2000 <yagzceritoglu@gmail.com>
2022-09-03 16:11:54 +0300

    feat: vite config edited due to publishing needs

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,40 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      reactivityTransform: true,
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    coverage: {
+      include: ["src/**/*.vue"],
+      reporter: ["lcov"],
+    },
+  },
+  build: {
+    minify: true,
+    lib: {
+      entry: resolve(__dirname, "src/index.js"),
+      name: "gallery-vue3",
+      fileName: (format) => `gallery-vue3.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        exports: "named",
+        assetFileNames: `gallery-vue3.[ext]`,
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    dedupe: ["vue"],
+  },
+});
